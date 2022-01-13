dcd71d2c6c7eb74b86de1923f616bda344d502b0
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:37:05 +0000

    libinit_hisi: Use pragma once
    Change-Id: I9a9e193a6f66f045f41394d059af8b71778e8d72

--- libinit_hisi/include/libinit_utils.h
+++ libinit_hisi/include/libinit_utils.h
@@ -4,11 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef LIBINIT_UTILS_H
-#define LIBINIT_UTILS_H
+#pragma once
 
 #include <string>
 
 void property_override(std::string prop, std::string value, bool add = true);
-
-#endif  // LIBINIT_UTILS_H
