a9f121ca3d7366b545ada90ef5f4b07ed92ef096
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-18 01:20:11 +0200

    hisi: vndk: Fix 32 bit libs paths
    Change-Id: Iea35b618f4d92e46132b00aba51f2d0989d941bc

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := prebuilts/vndk
 include $(CLEAR_VARS)
 LOCAL_MODULE := libprotobuf-cpp-full-v29
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_arm := v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so
+LOCAL_SRC_FILES_arm := v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so
 LOCAL_SRC_FILES_arm64 := v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -16,7 +16,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libprotobuf-cpp-lite-v29
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_arm := v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so
+LOCAL_SRC_FILES_arm := v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so
 LOCAL_SRC_FILES_arm64 := v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
