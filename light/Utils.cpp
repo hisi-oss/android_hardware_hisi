fa5228912ab77ab9b64aea34cdcbbefce4361f50
Giovanni Ricca <giovanniricca@proton.me>
2023-08-29 02:27:42 +0200

    hisi: light: Rebrand to hisi
    Change-Id: I7b9270f10ea2b38b35e789d2013c011eca33805d

--- light/Utils.cpp
+++ light/Utils.cpp
@@ -6,7 +6,7 @@
 
 #include "Utils.h"
 
-#define LOG_TAG "android.hardware.light-service.xiaomi"
+#define LOG_TAG "android.hardware.light-service.hisi"
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
