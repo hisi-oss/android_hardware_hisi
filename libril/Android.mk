0a94890d28ddb4c4a5409c3f570df4b96d663ed3
R0rt1z2 <me@r0rt1z2.com>
2024-02-02 23:21:18 +0100

    hisi: ril: Make the wrapper use `libril` instead
    * This makes the solution more generic and reduces duplicate code.

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,20 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(BOARD_USES_LIBRIL_WRAPPER),)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
     hardware/ril/include
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
-    libril \
-    libcom2ril
+    libril-hisi
 
-LOCAL_CFLAGS := -DLOG_TAG=\"libcommril\"
+LOCAL_CFLAGS := -DLOG_TAG=\"libril-wrapper\"
 
 LOCAL_SRC_FILES := \
-    commRil.cpp
+    ril.cpp
 
-LOCAL_MODULE := libcommril
+LOCAL_MODULE := libril
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+endif
