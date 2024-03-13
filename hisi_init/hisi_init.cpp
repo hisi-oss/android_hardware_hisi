f6461370f22f31735e66a3e01f8d01e4a3cc8a5a
Giovanni Ricca <giovanniricca@proton.me>
2024-03-13 22:07:01 +0100

    hisi_init: Initial `hisi_init` dummy binary
    * We'll use it to replicate `libinit` behaviour, but on vendor :D
    
    Change-Id: Idb7373b4bd90122258e2ec3a0e2fb5d280fad775

---
+++ hisi_init/hisi_init.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright (C) 2023 The LineageOS Project
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define LOG_TAG "hisi_init"
+
+#include <android-base/logging.h>
+
+int main() {
+}
