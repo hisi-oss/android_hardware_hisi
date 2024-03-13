5b46a33650346012c151d721621931c37529fe7e
R0rt1z2 <roger@r0rt1z2.com>
2024-03-13 22:16:13 +0100

    hisi_init: Introduce `hisi_connectivity`
    Squash of:
    
    Author: R0rt1z2 <me@r0rt1z2.com>
    Date: Thu, 22 Jun 2023 22:11:57 +0000
        hisi: Create vendor extension to configure chip types
    
        - This is manually done in stock /init.
    
        Change-Id: I29b1445d7fdaf701afef224c8e2e2169a8562710
    
    Author: R0rt1z2 <me@r0rt1z2.com>
    Date: Thu, 22 Jun 2023 22:44:09 +0000
        hisi: hisi_init: Load properties from `phone.prop`
    
        Change-Id: I23e87baf3e031304ee07b4eb3b82bc92e752465b
    
    Author: R0rt1z2 <roger@r0rt1z2.com>
    Date: Sun, 27 Aug 2023 14:44:15 +0000
        hisi: hisi_init: Load phone properties from `/odm` as well
    
        - Newer devices (i.e: Kirin 970) migrated to /odm in order to
          store phone-related properties. Since we support more than one
          platform, fallback to /vendor if /odm doesn't have phone.prop.
    
        Change-Id: Ib2ed075092bb0b382dc4f4e719f88608e49ba650
    
    Author: R0rt1z2 <roger@r0rt1z2.com>
    Date: Sun, 27 Aug 2023 14:54:23 +0000
        hisi: hisi_init: Don't set certain properties
    
        - While we're at it, get rid of the redundant logging in the
          SetPhoneProperties() function.
    
        Change-Id: Ib56cf2adae96b839457ac0cf013a65a3e04f57d1
    
    Author: Raphael Mounier <mounierr07@gmail.com>
    Date: Tue, 2 Jan 2024 16:46:13 +0100
        hisi: hisi_init: Return after setting `sys.rilprops_ready`
    
    Change-Id: Ic147e6f504f73bbcd68de4a135cf0216f48afadc

--- hisi_init/hisi_init.cpp
+++ hisi_init/hisi_init.cpp
@@ -1,12 +1,16 @@
-//
-// Copyright (C) 2023 The LineageOS Project
-//
-// SPDX-License-Identifier: Apache-2.0
-//
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #define LOG_TAG "hisi_init"
+
+#include "include/hisi_connectivity.h"
 
 #include <android-base/logging.h>
 
 int main() {
+    LOG(INFO) << "Running hisi_connectivity";
+    load_hisi_connectivity();
 }
